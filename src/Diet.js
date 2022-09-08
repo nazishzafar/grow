7384295f5248e6f2b05373401b7de2d26f1e7a71
nazishzafar <64459378+nazishzafar@users.noreply.github.com>
2022-09-08 16:19:38 +0500

    calculation

--- src/Diet.js
+++ src/Diet.js
@@ -1,54 +1,98 @@
 import React from 'react'
+import  { useState } from 'react'
 import nutrition from "./Images/Nutrition.svg"
 import { Button } from 'react-bootstrap';
 import "./styles/Home.css"
 
 const Diet = () => {
-  var name='';
-  var age=0;
-  var weight=0;
-  var height=0;
-  var gender='';
+  
+
+  const [userDiet, setUserDiet]=useState({
+     name:'',
+     age:0,
+     weight:0,
+     height:0,
+     gender:''
+  });
+
+  const handleInput=(e)=>{
+    const name=e.target.name;
+    const value=e.target.value;
+
+    setUserDiet({...userDiet,[name]: value})
+
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const BMI=userDiet.weight/Math.pow(userDiet.height,2)
+    var BEE;
+    if(userDiet.gender==="Male")
+    {
+       BEE=66.5 + (13.7* userDiet.weight) + (5.0* userDiet.height) - (6.8 * userDiet.age)
+    }else{
+       BEE=655.1 + (9.6* userDiet.weight) + (1.850 * ((userDiet.height)*100)) - (4.7 *userDiet.age)
+    }
+
+    if(BMI<18.5){
+      console.log("You're Underweight");
+    }
+    else if(BMI>18.5 && BMI <24.9){
+      console.log("Your weight is normal");
+    }
+    else if(BMI >25 && BMI < 29.9){
+      console.log("You're overweight");
+    }
+    else{
+      console.log("You're obese");
+    }
+  
+    console.log(userDiet)
+    console.log(BMI)
+    console.log(BEE)
+  }
+
   return (
     <div>
+      
       <div className='hero-image'>
       <img  src={nutrition} alt="hero image"/>
       </div>
       
 
-      <div>
+     
      
 
-      <form   className='card-container'>
+      <form onSubmit={handleSubmit}   className='card-container'>
 
         <div className='input-field'>
-          <label htmlFor='username'>Full Name</label>
-          <input type='text' autoComplete='off'  name='username' id='username' value={name}/>
+          <label htmlFor='name'>Full Name</label>
+          <input type='text' autoComplete='off'  name='name' id='name' onChange={handleInput} value={userDiet.name}/>
         </div>
 
         <div className='input-field'>
           <label htmlFor='age'> Age</label>
-          <input type='number' autoComplete='off'  name='age' id='age' value={age}/>
+          <input type='number' autoComplete='off'  name='age' id='age'  onChange={handleInput} value={userDiet.age}/>
         </div>
         <div className='input-field'>
           <label htmlFor='weight'>  Weight</label>
-          <input type='number'  autoComplete='off'  name='weight' id='weight' value={weight}/>
+          <input type='number'  autoComplete='off'  name='weight' id='weight' onChange={handleInput} value={userDiet.weight}/>
         </div>
 
         <div className='input-field'>
           <label htmlFor='Height'>  Height</label>
-          <input type='number'  autoComplete='off'  name='height' id='height' value={height}/>
+          <input type='number'  autoComplete='off'  name='height' id='height' onChange={handleInput} value={userDiet.height}/>
         </div>
         <div className='input-field'>
           <label htmlFor='gender'> Gender</label>
-          <input type='radio'  autoComplete='off'  name='gender' id='male' value='Male'/>Male
-          <input type='radio'  autoComplete='off'  name='gender' id='female' value='Female'/>Female
+          <input type='radio'  autoComplete='off' onChange={handleInput}   name='gender' id='male' value='Male'/>Male
+          <input type='radio'  autoComplete='off' onChange={handleInput}  name='gender' id='female' value='Female'/>Female
         </div>
 
         <Button  type='submit'>Submit</Button>
       </form>  
       </div>
-    </div>
+   
   )
 }
 
